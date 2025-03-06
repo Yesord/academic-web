881b9f51c848edba78a3a49c56a91f31aa6eb752
Mikazuki Tomoko <142188839+Yesord@users.noreply.github.com>
2025-03-06 15:59:38 +0800

    feat: update service icons and comment out unused bubble.js functionality

--- static/subsite/bubble/bubble.js
+++ static/subsite/bubble/bubble.js
@@ -8,11 +8,11 @@
       gravity: 0
     });
   
-    document.getElementById('gravity-btn').addEventListener('click', () => {
-      bg.spheres.config.gravity = bg.spheres.config.gravity === 0 ? 1 : 0;
-    });
+    // document.getElementById('gravity-btn').addEventListener('click', () => {
+    //   bg.spheres.config.gravity = bg.spheres.config.gravity === 0 ? 1 : 0;
+    // });
   
-    document.getElementById('colors-btn').addEventListener('click', () => {
-      bg.spheres.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()]);
-    });
+    // document.getElementById('colors-btn').addEventListener('click', () => {
+    //   bg.spheres.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()]);
+    // });
   })();
